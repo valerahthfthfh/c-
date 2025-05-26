cc721e5e47b9f26fca119d32b8744a5a7d406b42
valerahthfthfh <vl176932@gmail.com>
2025-05-26 18:30:13 +0700

    book

---
+++ Словари/Словари/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Slovari
+{
+    public class Book
+    {
+        public string name { get; set; }
+        public string author { get; set; }
+        public int year { get; set; }
+        public int id { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID - {id} ~ name - {name} ~ autor - {author} ~ year - {year}"; // meta
+        }
+    }
+
+    public class Library
+    {
+        private Dictionary<int, Book> books = new Dictionary<int, Book>();
+        private int nextId = 1;
+
+        // add book
+        public void addBook(string name, string author, int year)
+        {
+            var book = new Book
+            {
+                id = nextId++,
+                name = name,
+                author = author,
+                year = year
+            };
+            books.Add(book.id, book);
+            Console.WriteLine($"The book has been added");
+        }
+
+        // delete book
+        public bool deletBook(int id)
+        {
+            if (books.Remove(id))
+            {
+                Console.WriteLine("the book was deleted");
+                return true;
+            }
+            return false;
+        }
+
+        // search book
+        public Book searchBook(int id)
+        {
+            if (books.TryGetValue(id, out Book book))
+            {
+                return book;
+            }
+            return null;
+        }
+
+        // output oll book
+        public void outputBook()
+        {
+            Console.WriteLine("list of all books - ");
+            foreach (var book in books.Values)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+
+            // add
+            library.addBook("War and Peace", "Leo Tolstoy", 1869);
+            library.addBook("Crime and punishment", "Fyodor Dostoevsky", 1866);
+            library.addBook("The Master and Margarita", "Mikhail Bulgakov", 1967);
+
+            // output
+            library.outputBook();
+
+            // search
+            Book foundBook = library.searchBook(2);
+            if (foundBook != null)
+                Console.WriteLine($"Михаил Булгаков - {foundBook}");
+            // delete
+            library.deletBook(1);
+            // conclusion
+            library.outputBook();
+        }
+    }
+}
---
+++ Словари/Словари/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Словари")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Словари")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9f7df74b-52a3-48a3-bf0e-01b6f2a59855")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
